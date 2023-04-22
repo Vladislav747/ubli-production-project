dfed61179d0b0e5a9f41ed9cba011b193aedc7f3
vlad <zippo585@gmail.com>
2023-04-22 15:01:50 +0500

    PART 3

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -7,7 +7,7 @@ import {buildResolvers} from "./buildResolvers";
 import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode: mode,
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
-        devtool: "inline-source-map",
-        devServer: buildDevServer(options)
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -7,6 +7,11 @@ export interface BuildPaths {
     html: string;
 }
 
+export interface BuildEnv{
+    mode: BuildMode;
+    port: number;
+}
+
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
