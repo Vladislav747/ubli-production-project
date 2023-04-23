28c2998839f000a0383c20bd5c82d961d9ad61a8
vlad <zippo585@gmail.com>
2023-04-23 13:57:23 +0500

    PART 7

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import React, { Suspense } from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
+import '../../styles/index.scss';
 import {AboutPageAsync} from "../../pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "../../pages/MainPage/MainPage.async";
+import {useTheme} from "../../theme/useTheme";
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import {render} from "react-dom";
 import App from "./components/App/App";
 import {BrowserRouter} from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
     <BrowserRouter>
-    <App />,
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
 )
