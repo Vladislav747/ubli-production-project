ecaff29f1f63d1b67ef2df4171522c9a0b59d190
vlad <zippo585@gmail.com>
2023-05-17 16:38:21 +0500

    PART 24 - tests

--- src/shared/lib/classNames/classNames.test.js
+++ src/shared/lib/classNames/classNames.test.js
@@ -2,7 +2,7 @@ import { classNames } from './classNames';
 
 describe('classNames', () => {
     test('with only first param', () => {
-        expect(classNames('someClass')).toBe('someClass');
+        expect(classNames('someClass', {}, [])).toBe('someClass');
     });
 
     test('with additional class', () => {
