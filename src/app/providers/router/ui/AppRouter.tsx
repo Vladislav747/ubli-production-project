9c3222835d9b19b3978644eca132936efbc3eea1
vlad <zippo585@gmail.com>
2023-05-21 12:22:21 +0500

    PART 25-26

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,21 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
                 <Route
                     key={path}
                     path={path}
                     element={(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            {element}
+                        <Suspense fallback={<PageLoader />}>
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
                         </Suspense>
                     )}
                 />
             ))}
         </Routes>
-    </Suspense>
 );
 
 export default AppRouter;
