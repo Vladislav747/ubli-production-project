1f928070644484f5b397bc61d1fa1a0c9f065a50
vlad <zippo585@gmail.com>
2023-05-21 12:58:36 +0500

    PART 25-26

--- src/shared/ui/Button/Button.test.js
+++ src/shared/ui/Button/Button.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ThemeButton } from './Button';
 
 describe('Button', () => {
     test('Test render', () => {
