0ddb8d318209dd68f883caffab02e63ad5a8a657
vlad <zippo585@gmail.com>
2023-05-21 15:11:23 +0500

    PART 27

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
+import cls from 'src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className?: string;
--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -1,24 +1,34 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
-interface ISidebarProps {
-  className?: string;
+interface SidebarProps {
+    className?: string;
 }
 
-export const Sidebar = ({ className }: ISidebarProps) => {
+export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-    const handleToggle = () => {
+    const { t } = useTranslation();
+
+    const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
     return (
         <div
+            data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <button type="button" onClick={handleToggle}>toggle</button>
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                {t('Переключить')}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
