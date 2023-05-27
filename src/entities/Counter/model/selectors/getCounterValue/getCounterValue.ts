b1d3d47ef75e9bf922dd7e6f4e363ea493bf51c3
vlad <zippo585@gmail.com>
2023-05-27 23:39:23 +0500

    PART 30

--- src/entities/Counter/model/selectors/getCounterValue/getCounterValue.ts
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { getCounter } from '../getCounter/getCounter';
-import { CounterSchema } from 'entities/Counter';
+import { CounterSchema } from '../../types/counterSchema';
 
 export const getCounterValue = createSelector(
     getCounter,
--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,5 +1,5 @@
 import { counterReducer, counterActions } from './counterSlice';
-import { CounterSchema } from 'entities/Counter';
+import { CounterSchema } from '../types/counterSchema';
 
 describe('counterSlice.test', () => {
     test('decrement', () => {
