5204a020d75abffd21388594a0c12b4351809125
vlad <zippo585@gmail.com>
2023-05-27 23:31:37 +0500

    PART 30

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,6 +51,7 @@ module.exports = {
     'max-len': ['error', { ignoreComments: true, code: 100 }],
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'no-param-reassign': 'off',
   },
   globals: {
     __IS_DEV__: true,
