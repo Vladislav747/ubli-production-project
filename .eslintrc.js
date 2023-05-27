f29e8c93f89b1799a2a3dceb3d032dc14c66bd01
vlad <zippo585@gmail.com>
2023-05-27 21:11:05 +0500

    PART 29

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,6 +49,8 @@ module.exports = {
       },
     ],
     'max-len': ['error', { ignoreComments: true, code: 100 }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
   },
   globals: {
     __IS_DEV__: true,
