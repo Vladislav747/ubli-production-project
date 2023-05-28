2a1f7bad988caedcacef327eedb6962cabdfd8c4
vlad <zippo585@gmail.com>
2023-05-28 15:21:13 +0500

    PART 33 husky pre commit hook

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -5,13 +5,11 @@ export enum Theme {
     DARK = 'app_dark_theme',
 }
 
-
 export interface ThemeContextProps {
     theme?: Theme;
     setTheme?: (theme: Theme) => void;
 }
 
 export const ThemeContext = createContext<ThemeContextProps>({});
-
 
 export const LOCAL_STORAGE_THEME_KEY = 'theme';
