50fa1c53d56887ce219e45701aeba5c0f1314595
vlad <zippo585@gmail.com>
2023-05-28 14:45:33 +0500

    PART 33 husky pre commit hook

--- json-server/index.js
+++ json-server/index.js
@@ -34,6 +34,7 @@ server.post('/login', (req, res) => {
 
         return res.status(403).json({ message: 'User not found' });
     } catch (e) {
+
         console.log(e);
         return res.status(500).json({ message: e.message });
     }
