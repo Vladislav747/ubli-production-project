edf1cbbbef0a515200c54bc4db9b3ffcfd9572a4
vlad <zippo585@gmail.com>
2023-06-11 00:45:21 +0500

    PART 37

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { profileReducer } from '../../../entities/Profile';
 
 const reducers: ReducersList = {
     profile: profileReducer
