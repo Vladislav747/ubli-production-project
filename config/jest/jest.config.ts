2d14545d9f78fd155efa627faa6054020065f49a
vlad <zippo585@gmail.com>
2023-06-15 23:45:42 +0500

    PART 43

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -8,7 +8,8 @@ import path from 'path';
 export default {
     globals: {
         __IS_DEV__: true,
-        __API__: ''
+        __API__: '',
+        __PROJECT__: 'jest'
     },
     clearMocks: true,
     testEnvironment: 'jsdom',
--- src/shared/lib/tests/TestAsyncThunk/TestAsynkThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsynkThunk.ts
@@ -20,10 +20,13 @@ export class TestAsyncThunk<Return, Arg, RejectedValue> {
 
     navigate: jest.MockedFn<any>;
 
-    constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
+    constructor(
+        actionCreator: ActionCreatorType<Return, Arg, RejectedValue>,
+        state?: DeepPartial<StateSchema>
+    ) {
         this.actionCreator = actionCreator;
         this.dispatch = jest.fn();
-        this.getState = jest.fn();
+        this.getState = jest.fn(() => state as StateSchema);
 
         this.api = mockedAxios;
         this.navigate = jest.fn();
@@ -31,10 +34,11 @@ export class TestAsyncThunk<Return, Arg, RejectedValue> {
 
     async callThunk(arg: Arg) {
         const action = this.actionCreator(arg);
-        const result = await action(this.dispatch, this.getState, {
-            api: this.api,
-            navigate: this.navigate
-        });
+        const result = await action(
+            this.dispatch,
+            this.getState,
+            { api: this.api, navigate: this.navigate }
+        );
 
         return result;
     }
