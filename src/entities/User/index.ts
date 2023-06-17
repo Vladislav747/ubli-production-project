1203753d784603b906bffaf7ae5381a4e7376941
vlad <zippo585@gmail.com>
2023-06-18 01:16:44 +0500

    PART 48

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,11 +2,9 @@ export {
     getUserAuthData
 } from './model/selectors/getUserAuthData/getUserAuthData';
 
-
 export {
     getUserInitialized
-} from  '../../entities/User/model/selectors/getUserInitialized/getUserInitialized';
-
+} from '../../entities/User/model/selectors/getUserInitialized/getUserInitialized';
 
 export {
     userReducer,
