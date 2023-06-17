087fb38fee58a9d9b95fff3989e050ef4ea5e2a5
vlad <zippo585@gmail.com>
2023-06-17 19:05:26 +0500

    Fix tests

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -14,7 +14,9 @@ export const fetchProfileData = createAsyncThunk<
 
         try {
             const response = await extra.api.get<Profile>('/profile');
-
+            if (!response.data) {
+                throw new Error();
+            }
             return response.data;
         } catch (e) {
             console.log(e);
