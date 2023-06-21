60de33568abe8c0796e230e89121a2c7d4795603
vlad <zippo585@gmail.com>
2023-06-22 02:54:27 +0500

    PART 50

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import cls from './ArticlesPage.module.scss';
 
@@ -9,13 +8,8 @@ interface ArticlesPageProps {
 
 const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
 
-    return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            {t('ARTICLES PAGE')}
-        </div>
-    );
+    return <div className={classNames(cls.ArticlesPage, {}, [className])} />;
 };
 
 export default memo(ArticlesPage);
