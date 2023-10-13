6f0011ab760478813953ca610ec548f5d773f2f3
vlad <zippo585@gmail.com>
2023-10-13 22:55:29 +0500

    PART 123

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
 
 function getApiUrl(mode: BuildMode, apiUrl?: string) {
     if (apiUrl) {
